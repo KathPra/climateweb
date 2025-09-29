dcd2586312e0f86109d0c0c87a1b216a5103d43d
K. Prasse <katharina.prasse@posteo.de>
2025-09-29 22:58:37 +0200

    remove noise

--- format_mentions.py
+++ format_mentions.py
@@ -2,7 +2,7 @@ import csv
 import uuid
 
 tweets_file = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/tweet_attributes_final.csv" # contains only relevant tweets, no filtering needed
-formatted_mentions_file = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/formatted_mentions.csv"
+formatted_mentions_file = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/formatted_mentions_final.csv"
 
 with open(tweets_file, mode='r', encoding='utf-8') as infile, open(formatted_mentions_file, mode='w', encoding='utf-8', newline='') as outfile:
     reader = csv.DictReader(infile)
@@ -23,5 +23,15 @@ with open(tweets_file, mode='r', encoding='utf-8') as infile, open(formatted_men
         mention_id = uuid.uuid4()
         mentions = [word for word in text.split() if word.startswith('@')]
         for mention in mentions:
-            if len(mention) > 1:
-                writer.writerow([mention_id, id, mention[1:]])  # remove '@' from the mention
+            mention = mention.strip('.,!?;:"()[]{}')
+            mention = mention.split("http")[0]  # remove URLs if any
+            mention = mention.split("#")[0]  # remove hashtags if any
+            if mention.__contains__('@'):
+                mentions_faulty = mention.split('@')
+                for m in mentions_faulty:
+                    if len(m) > 1:
+                        m = m.strip('@')
+                        writer.writerow([mention_id, id, m])
+            else:
+                if len(mention) > 1:
+                    writer.writerow([mention_id, id, mention[1:]])  # remove '@' from the mention

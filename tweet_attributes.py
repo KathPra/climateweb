890daa6ac432657b70f357cc9180caa28832a0d9
K. Prasse <katharina.prasse@posteo.de>
2025-10-17 22:00:04 +0200

    add missing 2 tweets

--- tweet_attributes.py
+++ tweet_attributes.py
@@ -7,25 +7,15 @@ tweet_file3 = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/eng_2021_final.csv
 tweet_file4 = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/eng_2022_final.csv"
 
 # relevant tweets for ClimateTV
-url_2019_all = pickle.load(open("/ceph/lprasse/ClimateVisions/IMG_urls/final_IMG_urls_2019_all.pkl", "rb"))
-tweet_ids_2019 = {t.split("/")[-1].replace(".jpg","").split("_")[1] for t in url_2019_all}
-tweet_dict_2019 = {t.split("/")[-1].replace(".jpg","").split("_")[1]: t.split("/")[-1].replace(".jpg","") for t in url_2019_all}
-print(len(tweet_ids_2019))
+climatetv_final = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/climatetv_complete.txt"
 
-url_2020_all = pickle.load(open("/ceph/lprasse/ClimateVisions/IMG_urls/final_IMG_urls_2020_all.pkl", "rb"))
-tweet_ids_2020 = {t.split("/")[-1].replace(".jpg","").split("_")[1] for t in url_2020_all}
-tweet_dict_2020 = {t.split("/")[-1].replace(".jpg","").split("_")[1]: t.split("/")[-1].replace(".jpg","") for t in url_2020_all}
-print(len(tweet_ids_2020))
+## load required tweet ids
+tweet_dict = dict()
+with open(climatetv_final, "r", encoding='utf-8') as cf:
+    for line in cf:
+        tweet_dict[line.strip().split("_")[1]] = line.strip()  # tweet_id: image_id
+print(f"Total relevant tweets: {len(tweet_dict)}") # 2,412,535
 
-url_2021_all = pickle.load(open("/ceph/lprasse/ClimateVisions/IMG_urls/final_IMG_urls_2021_all.pkl", "rb"))
-tweet_ids_2021 = {t.split("/")[-1].replace(".jpg","").split("_")[1] for t in url_2021_all}
-tweet_dict_2021 = {t.split("/")[-1].replace(".jpg","").split("_")[1]: t.split("/")[-1].replace(".jpg","") for t in url_2021_all}
-print(len(tweet_ids_2021))
-
-url_2022_all = pickle.load(open("/ceph/lprasse/ClimateVisions/IMG_urls/final_IMG_urls_2022_all.pkl", "rb"))
-tweet_ids_2022 = {t.split("/")[-1].replace(".jpg","").split("_")[1] for t in url_2022_all}
-tweet_dict_2022 = {t.split("/")[-1].replace(".jpg","").split("_")[1]: t.split("/")[-1].replace(".jpg","") for t in url_2022_all}
-print(len(tweet_ids_2022))
 
 # prep ouptut file
 formatted_tweets_file = "/ceph/lprasse/ClimateVisions/ClimateWeb/inputs/tweet_attributes_final.csv"
@@ -36,17 +26,17 @@ with open(tweet_file1, mode='r', encoding='utf-8') as infile, open(formatted_twe
 
     # read the header
     header = next(reader)
-    #print("Original header:", header)
+    print("Original header:", header.keys())
     # change column names to "id", "reply_id", "tweet_id", "created_at", "text"
     new_header = ["conversation_id", "created_at", "text", "like_count", "retweet_count", "quote_count", "reply_count", "image_id"]
     writer.writerow(new_header)
 
     for row in reader:
         # copy from the original row
-        if row["id"] in tweet_ids_2019:  # filter for relevant tweets
-            writer.writerow([row["id"], row["created_at"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict_2019[row["id"]]])
+        if row["id"] in tweet_dict:  # filter for relevant tweets
+            writer.writerow([row["id"], row["created_at"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict[row["id"]]])
             counter_twids += 1
-            tweet_ids_2019.remove(row["id"])
+            tweet_dict.pop(row["id"])  # remove to save memory
 print("Relevant tweets in 2019:", counter_twids)
     
 
@@ -64,10 +54,10 @@ with open(tweet_file2, mode='r', encoding='utf-8') as infile, open(formatted_twe
 
     for row in reader:
         # copy from the original row
-        if row["id"] in tweet_ids_2020:  # filter for relevant tweets
-            writer.writerow([row["id"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict_2020[row["id"]]])
+        if row["id"] in tweet_dict:  # filter for relevant tweets
+            writer.writerow([row["id"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict[row["id"]]])
             counter_twids += 1
-            tweet_ids_2020.remove(row["id"])
+            tweet_dict.pop(row["id"])  # remove to save memory
 print("Relevant tweets in 2019 and 2020:", counter_twids)
 
 with open(tweet_file3, mode='r', encoding='utf-8') as infile, open(formatted_tweets_file, mode='a', encoding='utf-8', newline='') as outfile:
@@ -82,11 +72,11 @@ with open(tweet_file3, mode='r', encoding='utf-8') as infile, open(formatted_twe
     #writer.writerow(new_header)
 
     for row in reader:
-        if row["id"] in tweet_ids_2021:  # filter for relevant tweets
+        if row["id"] in tweet_dict:  # filter for relevant tweets
             # copy from the original row
-            writer.writerow([row["id"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict_2021[row["id"]]])
+            writer.writerow([row["id"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict[row["id"]]])
             counter_twids += 1
-            tweet_ids_2021.remove(row["id"])
+            tweet_dict.pop(row["id"])  # remove to save memory
 print("Relevant tweets in 2019,2020, and 2021:", counter_twids)
 
 with open(tweet_file4, mode='r', encoding='utf-8') as infile, open(formatted_tweets_file, mode='a', encoding='utf-8', newline='') as outfile:
@@ -101,9 +91,15 @@ with open(tweet_file4, mode='r', encoding='utf-8') as infile, open(formatted_twe
     #writer.writerow(new_header)
 
     for row in reader:
-        if row["id"] in tweet_ids_2022:  # filter for relevant tweets
+        if row["id"] in tweet_dict:  # filter for relevant tweets
             # copy from the original row
-            writer.writerow([row["id"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict_2022[row["id"]]])
+            writer.writerow([row["id"], row["text"], row["like_count"], row["retweet_count"], row["quote_count"], row["reply_count"], tweet_dict[row["id"]]])
             counter_twids += 1
-            tweet_ids_2022.remove(row["id"])
-print("Relevant tweets in 2019, 2020,2021 and 2022:", counter_twids)
+            tweet_dict.pop(row["id"])
+
+
+    print("Relevant tweets in 2019, 2020,2021 and 2022:", counter_twids) # 2,412,533
+    for twid in tweet_dict:
+        writer.writerow([twid, "", "", "", "", "", tweet_dict[twid]])
+        counter_twids += 1
+print("Total relevant tweets written:", counter_twids) # 2,412,535
